a49fb498663287dcb151ce8ff823716f78f2502c
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-09-19 14:30:01 -0400

    testing github login auth

--- src/components/LoginBar.jsx
+++ src/components/LoginBar.jsx
@@ -4,7 +4,7 @@ import './LoginBar.css';
 const LoginBar = () => {
     return (
         <div className="loginbar">
-            <NavButton link="/login" linkText="Login" variant="login" />
+            <NavButton link="/.auth/login/github" linkText="Login" variant="login" />
             <span className="titleText">Welcome to the Kubota Test Web App</span>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
             <DropdownButton
                 label="Store"
                 options={[
-                    { label: "Edit", onClick: () => console.log("Edit clicked"), link: "/edit" },
-                    { label: "Duplicate", onClick: () => console.log("Duplicate clicked"), link: "/duplicate" },
-                    { label: "Delete", onClick: () => console.log("Delete clicked"), link: "/delete" },
+                    { label: "Tractors", link: "/tractors" },
+                    { label: "Mowers", link: "/mowers" },
+                    { label: "Excavators", link: "/excavators" },
                 ]}
             />
             <NavButton link="/cart" linkText="Cart 🛒" variant="cart" />
